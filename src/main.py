ddce7b38b4d3b5919a40457054c1df0ff4adf32a
Adil Rahman <itsadil2022@gmail.com>
2026-04-30 15:49:48 +0600

    Added multiply function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 ## Write a simple program:
 
@@ -11,6 +11,8 @@ print("Date: 30-04-2026")
 ## calling functions from utils.py
 add_result = add(5, 3)
 subtract_result = subtract(10, 4)
+multiply_result = multiply(6, 7)
 
 print(f"Addition Result: {add_result}")
 print(f"Subtraction Result: {subtract_result}")
+print(f"Multiplication Result: {multiply_result}")
--- src/utils.py
+++ src/utils.py
@@ -7,5 +7,8 @@ def add(a, b):
 # subtract(a, b)
 def subtract(a, b):
     return a - b
+# multiply(a, b)
+def multiply(a, b):
+    return a * b
 
 # Call them main file.
